cbdb543d23e5e68cbca29121b586cb0a3a31e1e7
Gabyte225 <gabe300@gmail.com>
2022-09-26 21:15:45 +0300

    countdown clock

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+//drag'n'drop buttons:
 const addBtns = document.querySelectorAll(".add-btn:not(.solid)");
 const saveItemBtns = document.querySelectorAll(".solid");
 const addTaskContainers = document.querySelectorAll(".add-container");
-const addTask = document.querySelectorAll("#add-task");
+const addTask = document.querySelectorAll(".add-task");
 const addTaskButton = document.querySelectorAll(".add-task-button");
-
+//drag'n'drop inputs:
 const listColumns = document.querySelectorAll(".drag-item-list");
 const todoListEl = document.querySelector("#todo-list");
 const progressListEl = document.querySelector("#progress-list");
 const completeListEl = document.querySelector("#complete-list");
 const notSureListEl = document.querySelector("#not-sure-list");
+//countdown form:
+const countdownForm = document.getElementById("countdown-form");
+const inputContainer = document.getElementById("input-container");
+const dateEl = document.getElementById("date-picker");
+//countdown clock:
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("countdown-title");
+const countdownBtn = document.getElementById("countdown-button");
+const timeElements = document.querySelectorAll(".span-list");
+//countdown complete:
+const completeEl = document.getElementById("complete");
+const completeElInfo = document.getElementById("complete-info");
+const completeBtn = document.getElementById("complete-button");
 
+//Drag'n'drop logic:
 let updatedOnLoad = false;
 
 let todoListArray = [];
@@ -58,7 +73,7 @@ function createItemEl(columnEl, column, item, index) {
   listEl.draggable = true;
   listEl.id = index;
   listEl.classList.add("drag-item");
-  listEl.setAttribute('ondragstart', 'drag(event)');
+  listEl.setAttribute("ondragstart", "drag(event)");
   listEl.contentEditable = true;
   columnEl.appendChild(listEl);
 }
@@ -99,8 +114,11 @@ function updateDOM() {
 function addToColumn(column) {
   const itemText = addTask[column].value;
   const selectedArray = listArrays[column];
-  selectedArray.push(itemText);
-  addTask[column].value = "";
+  if (addTask[column].value != "") {
+    addTask[column].value = '';
+    selectedArray.push(itemText);
+  }
+
   updateDOM(column);
 }
 addTaskButton.forEach((button, i) =>
@@ -147,9 +165,8 @@ function rebuildArrays() {
 }
 
 function dragEnter(column) {
-    listColumns[column].classList.add('over');
-    currentColumn = column;
-  }
+  currentColumn = column;
+}
 
 function drag(e) {
   draggedItem = e.target;
@@ -172,3 +189,92 @@ function drop(e) {
 }
 
 updateDOM();
+
+//Countdown logic:
+let countdownTitle = "";
+let countdownDate = "";
+let countdownValue = Date;
+let countdownActive;
+let savedCountdown;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+const today = new Date().toISOString().split("T")[0];
+dateEl.setAttribute("min", today);
+
+function updateDOC() {
+  countdownActive = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
+    const days = Math.floor(distance / day);
+    const hours = Math.floor((distance % day) / hour);
+    const minutes = Math.floor((distance % hour) / minute);
+    const seconds = Math.floor((distance % minute) / second);
+    inputContainer.hidden = true;
+    if (distance < 0) {
+      countdownEl.hidden = true;
+      clearInterval(countdownActive);
+      completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`;
+      completeEl.hidden = false;
+      todoListArray = [];
+      progressListArray = [];
+      completeListArray = [];
+      notSureListArray = [];
+      updateDOM();
+    } else {
+      countdownElTitle.textContent = `${countdownTitle}`;
+      timeElements[0].textContent = `${days}`;
+      timeElements[1].textContent = `${hours}`;
+      timeElements[2].textContent = `${minutes}`;
+      timeElements[3].textContent = `${seconds}`;
+      completeEl.hidden = true;
+      countdownEl.hidden = false;
+    }
+  }, second);
+}
+
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownDate = e.srcElement[0].value;
+  savedCountdown = {
+    title: countdownTitle,
+    date: countdownDate,
+  };
+  localStorage.setItem("countdown", JSON.stringify(savedCountdown));
+  if (countdownDate === "") {
+    alert("Please select a date for sprint countdown.");
+  } else {
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOC();
+  }
+}
+
+function reset() {
+  countdownEl.hidden = true;
+  completeEl.hidden = true;
+  inputContainer.hidden = false;
+  clearInterval(countdownActive);
+  countdownTitle = "";
+  countdownDate = "";
+  localStorage.removeItem("countdown");
+}
+
+function setCountdown() {
+  if (localStorage.getItem("countdown")) {
+    inputContainer.hidden = true;
+    savedCountdown = JSON.parse(localStorage.getItem("countdown"));
+    countdownTitle = savedCountdown.title;
+    countdownDate = savedCountdown.date;
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOC();
+  }
+}
+
+countdownForm.addEventListener("submit", updateCountdown);
+countdownBtn.addEventListener("click", reset);
+completeBtn.addEventListener("click", reset);
+
+setCountdown();
